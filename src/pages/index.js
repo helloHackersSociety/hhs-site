b1ab41ac6fadc50879d7e9809c430e4bd47a5603
tomMisson <11tmisson@gmail.com>
2020-05-05 16:33:47 +0100

    Cut out the crap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      {/* <Image /> */}
-    </div>
+      <h1><span role="img" aria-label="waving hand emoji">👋</span> Hello hacker! </h1>
   </Layout>
 )
 
