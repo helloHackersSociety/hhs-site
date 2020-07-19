be5149dbb6dc29222c3f45e71fb973112b1c3366
TomMisson <11tmisson@gmail.com>
2020-07-19 20:39:25 +0100

    Get rid of unused stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -35,8 +33,5 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-netlify`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
